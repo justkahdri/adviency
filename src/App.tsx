c545d0769e727bc60a2ac334edb9436466355d75
JustKahdri <joaco.rm512@gmail.com>
2021-12-07 13:47:46 -0300

    day 5 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import React from "react";
+import type {MutableRefObject} from "react";
+
 import "./styles.css";
 
 export default function App() {
+  const [presents, setPresents] = React.useState<string[]>(["Auto de juguete"]);
+  const inputPresent = React.useRef() as MutableRefObject<HTMLInputElement>;
+
   return (
-    <div className="App">
-      <h1>Regalos:</h1>
-      ...
-    </div>
+    <main className="App">
+      <div className="container">
+        <h1>Regalos:</h1>
+        <section className="fake-form">
+          <input ref={inputPresent} placeholder="Nombre de regalo" type="text" />
+          <button
+            type="button"
+            onClick={() => setPresents([inputPresent?.current.value, ...presents])}
+          >
+            Agregar!
+          </button>
+        </section>
+        <section className="presents-section">
+          <ul>
+            {presents.map(function (present: string) {
+              return (
+                <ol
+                  key={present}
+                  onClick={() => setPresents((ps) => ps.filter((p) => p !== present))}
+                >
+                  {present}
+                </ol>
+              );
+            })}
+          </ul>
+        </section>
+        <button onClick={() => setPresents([])}>Borrar todos los regalos 😥</button>
+      </div>
+    </main>
   );
 }
